58b820117188f91da1f84b8fe83c3d5a22e9941b
Freshloic <loicjeancharles@gmail.com>
2018-01-27 17:45:08 -0400

    composer partie

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -23,17 +23,82 @@ class AdvertController extends Controller
     // Ici, on récupérera la liste des annonces, puis on la passera au template
 
     // Mais pour l'instant, on ne fait qu'appeler le template
-    return $this->render('OCPlatformBundle:Advert:index.html.twig');
+    $listAdverts = array(
+
+  array(
+
+    'title'   => 'Recherche développpeur Symfony',
+
+    'id'      => 1,
+
+    'author'  => 'Alexandre',
+
+    'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+
+    'date'    => new \Datetime()),
+
+  array(
+
+    'title'   => 'Mission de webmaster',
+
+    'id'      => 2,
+
+    'author'  => 'Hugo',
+
+    'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+
+    'date'    => new \Datetime()),
+
+  array(
+
+    'title'   => 'Offre de stage webdesigner',
+
+    'id'      => 3,
+
+    'author'  => 'Mathieu',
+
+    'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+
+    'date'    => new \Datetime())
+
+);
+
+
+// Et modifiez le 2nd argument pour injecter notre liste
+
+return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+
+  'listAdverts' => $listAdverts
+
+));
   }
 
   public function viewAction($id)
-  {
-    // Ici, on récupérera l'annonce correspondante à l'id $id
 
-    return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
-      'id' => $id
-    ));
-  }
+    {
+
+      $advert = array(
+
+        'title'   => 'Recherche développpeur Symfony2',
+
+        'id'      => $id,
+
+        'author'  => 'Alexandre',
+
+        'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+
+        'date'    => new \Datetime()
+
+      );
+
+
+      return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+
+        'advert' => $advert
+
+      ));
+
+    }
 
   public function addAction(Request $request)
   {
@@ -61,10 +126,29 @@ class AdvertController extends Controller
     if ($request->isMethod('POST')) {
       $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
 
-      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+      return $this->redirectToRoute('oc_platform_view', array('id' => $id));
     }
 
-    return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+    $advert = array(
+
+     'title'   => 'Recherche développpeur Symfony',
+
+     'id'      => $id,
+
+     'author'  => 'Alexandre',
+
+     'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+
+     'date'    => new \Datetime()
+
+   );
+
+
+   return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
+
+     'advert' => $advert
+
+   ));
   }
 
   public function deleteAction($id)
@@ -73,6 +157,56 @@ class AdvertController extends Controller
 
     // Ici, on gérera la suppression de l'annonce en question
 
-    return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+    $advert = array(
+
+     'title'   => 'Recherche développpeur Symfony',
+
+     'id'      => $id,
+
+     'author'  => 'Alexandre',
+
+     'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+
+     'date'    => new \Datetime()
+
+   );
+
+
+   return $this->render('OCPlatformBundle:Advert:delete.html.twig', array(
+
+     'advert' => $advert
+
+   ));
   }
+
+  public function menuAction($limit)
+
+{
+
+  // On fixe en dur une liste ici, bien entendu par la suite
+
+  // on la récupérera depuis la BDD !
+
+  $listAdverts = array(
+
+    array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+
+    array('id' => 5, 'title' => 'Mission de webmaster'),
+
+    array('id' => 9, 'title' => 'Offre de stage webdesigner')
+
+  );
+
+
+  return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+
+    // Tout l'intérêt est ici : le contrôleur passe
+
+    // les variables nécessaires au template !
+
+    'listAdverts' => $listAdverts
+
+  ));
+
+}
 }
