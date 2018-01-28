5fb037b685aa72d9af0cd4f0fb3cc12285bbd6ba
Freshloic <loicjeancharles@gmail.com>
2018-01-27 23:42:38 -0400

    utilisation d'un ORM

---
+++ src/OC/PlatformBundle/Entity/Commande.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace OC\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Commande
+ *
+ * @ORM\Table(name="oc_commande")
+ * @ORM\Entity(repositoryClass="OC\PlatformBundle\Repository\CommandeRepository")
+ */
+class Commande
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="ListeProduits", type="array")
+     */
+    private $listeProduits;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="AdresseLivraison", type="string", length=255)
+     */
+    private $adresseLivraison;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="Date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set listeProduits
+     *
+     * @param array $listeProduits
+     *
+     * @return Commande
+     */
+    public function setListeProduits($listeProduits)
+    {
+        $this->listeProduits = $listeProduits;
+
+        return $this;
+    }
+
+    /**
+     * Get listeProduits
+     *
+     * @return array
+     */
+    public function getListeProduits()
+    {
+        return $this->listeProduits;
+    }
+
+    /**
+     * Set adresseLivraison
+     *
+     * @param string $adresseLivraison
+     *
+     * @return Commande
+     */
+    public function setAdresseLivraison($adresseLivraison)
+    {
+        $this->adresseLivraison = $adresseLivraison;
+
+        return $this;
+    }
+
+    /**
+     * Get adresseLivraison
+     *
+     * @return string
+     */
+    public function getAdresseLivraison()
+    {
+        return $this->adresseLivraison;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Commande
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Get prixTotal
+     *
+     * @return int
+     */
+    public function getPrixTotal()
+    {
+
+      $prix = 0;
+
+      foreach($this->getListeProduits() as $produit) {
+
+        $prix += $produit->getPrix();
+
+      }
+
+      return $prix;
+
+    }
+}
---
+++ src/OC/PlatformBundle/Entity/Product.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace OC\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="oc_product")
+ * @ORM\Entity(repositoryClass="OC\PlatformBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="prix", type="integer")
+     */
+    private $prix;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Product
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param integer $prix
+     *
+     * @return Product
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return int
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+}
